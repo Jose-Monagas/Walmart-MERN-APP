f6de35dc2d0675d7f101dff2448cae052f7de3b7
SriD647 <sridatt.rao@hotmail.com>
2023-08-28 16:09:45 -0400

    added routes for users, department, and orders

---
+++ Fallmart/routes/api/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const { checkToken, dataController, apiController } = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
+
+// POST /api/users
+router.post('/', dataController.create, apiController.auth)
+// POST /api/users/login
+router.post('/login', dataController.login, apiController.auth)
+// PUT /api/users/update
+router.put('/update', dataController.update, apiController.auth)
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, checkToken)
+
+module.exports = router
