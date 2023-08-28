fb556afe349b99ee37f690870d8a7b9166267a73
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-08-28 16:43:42 -0400

    added server file

--- Fallmart/models/review.js
+++ Fallmart/models/review.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema({
 name:{type:String,default:'Anonymous'},
 rating:{type:Number,min:1,max:5,default:0},
-product:{type:Schema.Types.ObjectId,ref:'Product'}
+product:{type:Schema.Types.ObjectId,ref:'Product'},
+user:{type:Schema.Types.ObjectId,ref:'User'}
 })
 
 module.exports = mongoose.model("review",reviewSchema)
