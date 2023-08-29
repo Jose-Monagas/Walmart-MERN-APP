eb8afc21fee1e1694e8d5d7e7780eeff6856b356
joecruz17 <joejcruz17@gmail.com>
2023-08-29 19:37:47 -0400

    seed file update

--- Fallmart/config/seed.js
+++ Fallmart/config/seed.js
@@ -3,5 +3,269 @@ require('./database');
 
 const Department = require('../models/department');
 const subCategory = require('../models/subCategory')
+const Product = require('../../product')
 
-//going to put the array of items here
+(async function() {
+
+    await Department.deleteMany({});
+    const depts = await Department.create([
+      {name: 'Groceries', sortOrder: 10}, //0
+      {name: 'Personal Care', sortOrder: 20}, //1
+      {name: 'Electronics', sortOrder: 30}, //2
+      {name: 'Home, Furniture', sortOrder: 40}, //3
+      {name: 'Pets', sortOrder: 50}, //4
+      {name: 'Baby', sortOrder: 60}, //5
+      {name: 'Clothing', sortOrder: 70}, //6
+    ]);
+
+await Item.deleteMany({});
+
+const Products = await Product.create([
+    {
+        name: "Apples",
+        price: 2.99,
+        image: "",
+        manufacturer: "Farm Fresh",
+        department: "Groceries",
+        description: "Fresh and delicious apples.",
+        review: "",
+        subcategory: "Fruits"
+      },
+      {
+        name: "Shampoo",
+        price: 5.49,
+        image: "",
+        manufacturer: "CareClean",
+        department: "Personal Care",
+        description: "Gentle and nourishing shampoo.",
+        review: "",
+        subcategory: "Hair Care"
+      },
+      {
+        name: "Smartphone",
+        price: 399.99,
+        image: "",
+        manufacturer: "Techtronics",
+        department: "Electronics",
+        description: "High-end smartphone with advanced features.",
+        review: "",
+        subcategory: "Mobile Phones"
+      },
+      {
+        name: "Sofa",
+        price: 299.00,
+        image: "",
+        manufacturer: "ComfyLiving",
+        department: "Home Furniture",
+        description: "Comfortable and stylish sofa for your living room.",
+        review: "",
+        subcategory: "Living Room Furniture"
+      },
+      {
+        name: "Dog Food",
+        price: 12.99,
+        image: "",
+        manufacturer: "PetPal",
+        department: "Pets",
+        description: "Nutritious dog food for your furry friend.",
+        review: "",
+        subcategory: "Pet Food"
+      },
+      {
+        name: "Baby Diapers",
+        price: 19.95,
+        image: "",
+        manufacturer: "TinyTots",
+        department: "Baby",
+        description: "Soft and absorbent diapers for babies.",
+        review: "",
+        subcategory: "Diapers"
+      },
+      {
+        name: "T-Shirt",
+        price: 9.99,
+        image: "",
+        manufacturer: "FashionFab",
+        department: "Clothing",
+        description: "Casual and comfortable cotton T-shirt.",
+        review: "",
+        subcategory: "Casual Wear"
+      },
+      {
+        name: "Orange Juice",
+        price: 3.49,
+        image: "",
+        manufacturer: "JuicyCo",
+        department: "Groceries",
+        description: "Freshly squeezed orange juice.",
+        review: "",
+        subcategory: "Beverages"
+      },
+      {
+        name: "Toothpaste",
+        price: 2.29,
+        image: "",
+        manufacturer: "DentalCare",
+        department: "Personal Care",
+        description: "Fluoride toothpaste for strong teeth.",
+        review: "",
+        subcategory: "Oral Care"
+      },
+      {
+        name: "Laptop",
+        price: 799.00,
+        image: "",
+        manufacturer: "TechGenius",
+        department: "Electronics",
+        description: "Powerful laptop for work and entertainment.",
+        review: "",
+        subcategory: "Computers"
+      },
+      {
+        name: "Coffee Table",
+        price: 149.99,
+        image: "",
+        manufacturer: "FurnishHome",
+        department: "Home Furniture",
+        description: "Elegant coffee table for your living room.",
+        review: "",
+        subcategory: "Living Room Furniture"
+      },
+      {
+        name: "Cat Food",
+        price: 8.75,
+        image: "",
+        manufacturer: "KittyCuisine",
+        department: "Pets",
+        description: "Delicious cat food for your feline companion.",
+        review: "",
+        subcategory: "Pet Food"
+      },
+      {
+        name: "Baby Wipes",
+        price: 6.99,
+        image: "",
+        manufacturer: "TinyTots",
+        department: "Baby",
+        description: "Gentle baby wipes for easy cleaning.",
+        review: "",
+        subcategory: "Diapering"
+      },
+      {
+        name: "Dress Shirt",
+        price: 24.95,
+        image: "",
+        manufacturer: "ElegantAttire",
+        department: "Clothing",
+        description: "Formal dress shirt for special occasions.",
+        review: "",
+        subcategory: "Formal Wear"
+      },
+
+      {
+        name: "Bananas",
+        price: 1.49,
+        image: "",
+        manufacturer: "TropicalHarvest",
+        department: "Groceries",
+        description: "Sweet and nutritious bananas.",
+        review: "",
+        subcategory: "Fruits"
+      },
+      {
+        name: "Conditioner",
+        price: 4.99,
+        image: "",
+        manufacturer: "SilkyStrands",
+        department: "Personal Care",
+        description: "Smooth and hydrating hair conditioner.",
+        review: "",
+        subcategory: "Hair Care"
+      },
+      {
+        name: "Tablet",
+        price: 199.00,
+        image: "",
+        manufacturer: "TechWonder",
+        department: "Electronics",
+        description: "Versatile tablet for work and entertainment.",
+        review: "",
+        subcategory: "Tablets"
+      },
+      {
+        name: "Desk",
+        price: 149.50,
+        image: "",
+        manufacturer: "StudySolutions",
+        department: "Home Furniture",
+        description: "Functional desk for your home office.",
+        review: "",
+        subcategory: "Office Furniture"
+      },
+      {
+        name: "Cat Litter",
+        price: 9.25,
+        image: "",
+        manufacturer: "KittyCuisine",
+        department: "Pets",
+        description: "Odor-absorbing cat litter for easy cleanup.",
+        review: "",
+        subcategory: "Pet Supplies"
+      },
+      {
+        name: "Baby Formula",
+        price: 17.99,
+        image: "",
+        manufacturer: "TinyTots",
+        department: "Baby",
+        description: "Nutrient-rich baby formula for infants.",
+        review: "",
+        subcategory: "Feeding"
+      },
+      {
+        name: "Jeans",
+        price: 29.95,
+        image: "",
+        manufacturer: "DenimDeluxe",
+        department: "Clothing",
+        description: "Classic denim jeans for everyday wear.",
+        review: "",
+        subcategory: "Jeans"
+      },
+      {
+        name: "Peaches",
+        price: 3.29,
+        image: "",
+        manufacturer: "OrchardFresh",
+        department: "Groceries",
+        description: "Juicy and flavorful peaches.",
+        review: "",
+        subcategory: "Fruits"
+      },
+      {
+        name: "Deodorant",
+        price: 3.75,
+        image: "",
+        manufacturer: "FreshGlow",
+        department: "Personal Care",
+        description: "Long-lasting deodorant with a pleasant scent.",
+        review: "",
+        subcategory: "Personal Care"
+      },
+      {
+        name: "Bluetooth Speaker",
+        price: 59.99,
+        image: "",
+        manufacturer: "SonicSound",
+        department: "Electronics",
+        description: "Compact Bluetooth speaker for wireless audio.",
+        review: "",
+        subcategory: "Speakers"
+      }
+
+    
+]);
+
+process.exit();
+
+});
---
+++ Fallmart/public/js/dist/App.61c71311c065051d03ba.js
@@ -0,0 +1,261 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
+/* harmony import */ var _router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./router */ "./src/router/index.js");
+/* provided dependency */ var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+
+
+const root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(document.getElementById("app"));
+root.render( /*#__PURE__*/React.createElement(react__WEBPACK_IMPORTED_MODULE_0__.StrictMode, null, /*#__PURE__*/React.createElement(_router__WEBPACK_IMPORTED_MODULE_2__["default"], null)));
+
+/***/ }),
+
+/***/ "./src/router/index.js":
+/*!*****************************!*\
+  !*** ./src/router/index.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../components/NavBar'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/dist/index.js");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/dist/index.js");
+/* harmony import */ var _routes__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./routes */ "./src/router/routes.js");
+/* provided dependency */ var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+
+
+const AppRouter = () => {
+  return /*#__PURE__*/React.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__.BrowserRouter, null, /*#__PURE__*/React.createElement(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../components/NavBar'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()), {
+    routes: _routes__WEBPACK_IMPORTED_MODULE_1__["default"]
+  }), /*#__PURE__*/React.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_3__.Routes, null, _routes__WEBPACK_IMPORTED_MODULE_1__["default"].map(_ref => {
+    let {
+      Component,
+      key,
+      path
+    } = _ref;
+    return /*#__PURE__*/React.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_3__.Route, {
+      key: key,
+      path: path,
+      element: /*#__PURE__*/React.createElement(Component, {
+        page: key
+      })
+    });
+  })));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AppRouter);
+
+/***/ }),
+
+/***/ "./src/router/routes.js":
+/*!******************************!*\
+  !*** ./src/router/routes.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/About'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/Home'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/Contact'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+
+const routes = [{
+  Component: Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/Contact'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()),
+  key: 'Contact',
+  path: '/contact'
+}, {
+  Component: Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/Home'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()),
+  key: 'Home',
+  path: '/'
+}, {
+  Component: Object(function webpackMissingModule() { var e = new Error("Cannot find module '../pages/About'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()),
+  key: 'About',
+  path: '/about'
+}];
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (routes);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			id: moduleId,
+/******/ 			loaded: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/node module decorator */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nmd = (module) => {
+/******/ 			module.paths = [];
+/******/ 			if (!module.children) module.children = [];
+/******/ 			return module;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"App": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkbig_poppa_code_react_starter_kit"] = self["webpackChunkbig_poppa_code_react_starter_kit"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_react-dom_client_js-node_modules_react-router-dom_dist_index_js"], () => (__webpack_require__("./src/index.js")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=App.4ef3839e05fff6a4651fb1abb3ec0a9b.js.map
