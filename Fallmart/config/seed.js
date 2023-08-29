ad900b636143ca96a6859cd6f1eff11dfa9606e1
joecruz17 <joejcruz17@gmail.com>
2023-08-28 20:48:36 -0400

    starting the seed file

---
+++ Fallmart/config/seed.js
@@ -0,0 +1,7 @@
+// require('dotenv').config();
+require('./database');
+
+const Department = require('../models/department');
+const subCategory = require('../models/subCategory')
+
+//going to put the array of items here
