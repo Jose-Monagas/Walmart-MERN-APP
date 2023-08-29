b5a2dc8da5e23cb722d304e192617f6341327dd1
Graehm <gpatrellfazio@gmail.com>
2023-08-28 20:48:12 -0500

    search function added, fixed file structure

--- Fallmart/src/components/NavBar.js
+++ Fallmart/src/components/NavBar.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = props => {
-	return (
-		<nav className="NavBar">
-			{props.routes.map(({ key, path }) => (
-				<Link key={key} to={path}>
-					{key}
-				</Link>
-			))}
-		</nav>
-	);
+  const [search, setSearch] = useState('');
+
+  return (
+    <nav className="NavBar">
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {props.routes.map(({ key, path }) => (
+        <Link key={key} to={path}>
+          {key}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default NavBar;
