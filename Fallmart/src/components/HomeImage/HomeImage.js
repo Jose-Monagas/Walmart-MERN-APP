c233eec486f75266ad28092e133c09922b37ce17
jib5549 <jib5549@gmail.com>
2023-08-30 15:29:18 -0400

    everything is rendering perfectly now

--- Fallmart/src/components/HomeImage/HomeImage.js
+++ Fallmart/src/components/HomeImage/HomeImage.js
@@ -5,13 +5,11 @@ function HomeImage() {
 	return (
 		<div className={styles.allimage}>
 			<img
-				className={styles.first_fullImg}
 				src="https://i.imgur.com/eflVxXr.jpg"
 				alt=""
 				// width="100%"
 			/>
 			<img
-				className={styles.second_fullImg}
 				src="https://i.imgur.com/frxnxYF.jpg"
 				alt=""
 				// width="100%"
