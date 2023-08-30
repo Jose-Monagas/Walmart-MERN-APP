8e4003d7a41ef340c784f65257ebed5d8a7f1f05
Jose <josemonagas212@gmail.com>
2023-08-29 20:44:10 -0400

    feat: update imports

--- Fallmart/src/router/index.js
+++ Fallmart/src/router/index.js
@@ -1,10 +1,10 @@
-import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar/NavBar';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import routes from './routes';
 const AppRouter = () => {
 	return (
 		<Router>
-			<NavBar routes={routes} />	
+			<NavBar routes={routes} />
 			<Routes>
 				{routes.map(({ Component, key, path }) => (
 					<Route
