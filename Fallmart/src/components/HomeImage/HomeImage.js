5cd0639cc44ae944e43d00df09aa1231a4a9db90
Jose <josemonagas212@gmail.com>
2023-08-29 20:37:59 -0400

    fix: convert .css files to .module.scss files

--- Fallmart/src/components/HomeImage/HomeImage.js
+++ Fallmart/src/components/HomeImage/HomeImage.js
@@ -1,23 +1,23 @@
-import React from "react";
-import styles from "./HomeImage.css";
+import React from 'react';
+import styles from './HomeImage.module.scss';
 
 function HomeImage() {
-  return (
-    <div className="allimage">
-      <img
-        className="first_fullImg"
-        src="//cb.scene7.com/is/image/Crate/cb_dHP_08242023_CBCC_Hero?wid=1440&amp;qlt=80&amp;op_sharpen=1"
-        alt=""
-        width="100%"
-      />
-      <img
-        className="second_fullImg"
-        src="//cb.scene7.com/is/image/Crate/cb_dHP_08242023_FurniturePromo?wid=1440&amp;qlt=80&amp;op_sharpen=1"
-        alt=""
-        width="100%"
-      />
-    </div>
-  );
+	return (
+		<div className="allimage">
+			<img
+				className="first_fullImg"
+				src="//cb.scene7.com/is/image/Crate/cb_dHP_08242023_CBCC_Hero?wid=1440&amp;qlt=80&amp;op_sharpen=1"
+				alt=""
+				width="100%"
+			/>
+			<img
+				className="second_fullImg"
+				src="//cb.scene7.com/is/image/Crate/cb_dHP_08242023_FurniturePromo?wid=1440&amp;qlt=80&amp;op_sharpen=1"
+				alt=""
+				width="100%"
+			/>
+		</div>
+	);
 }
 
 export default HomeImage;
