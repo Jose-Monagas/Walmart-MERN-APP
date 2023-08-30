8840a07e71098961dd3a147588e6a5c799429b49
Jose <josemonagas212@gmail.com>
2023-08-29 20:44:29 -0400

    fix: get router working with basic config

--- Fallmart/src/router/routes.js
+++ Fallmart/src/router/routes.js
@@ -1,22 +1,10 @@
-import About from '../pages/About';
-import Home from '../pages/Home';
-import Contact from '../pages/Contact';
+import Fallmart from '../pages/Fallmart/Fallmart';
 
 const routes = [
 	{
-		Component: Contact,
-		key: 'Contact',
-		path: '/contact'
-	},
-	{
-		Component: Home,
-		key: 'Home',
+		Component: Fallmart,
+		key: 'Fallmart',
 		path: '/'
-	},
-	{
-		Component: About,
-		key: 'About',
-		path: '/about'
 	}
 ];
 
