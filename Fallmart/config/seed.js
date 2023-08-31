f6a073040511f13df26f5de765882f7fd4bce7f6
joecruz17 <joejcruz17@gmail.com>
2023-08-30 21:51:07 -0400

    seed issues fixed, now just filling in the rest of the items and subcategories

--- Fallmart/config/seed.js
+++ Fallmart/config/seed.js
@@ -2,270 +2,276 @@ require('dotenv').config();
 require('./database');
 
 const Department = require('../models/department');
-// const subCategory = require('../models/subCategory')
-const Product = require('../models/product')
-
-(async function() {
-
+const SubCategory = require('../models/subCategory');
+const Product = require('../models/product');
+(async function (){
     await Department.deleteMany({});
-    const depts = await Department.create([
-      {name: 'Groceries', sortOrder: 10}, //0
-      {name: 'Personal Care', sortOrder: 20}, //1
-      {name: 'Electronics', sortOrder: 30}, //2
-      {name: 'Home & Furniture', sortOrder: 40}, //3
-      {name: 'Pets', sortOrder: 50}, //4
-      {name: 'Baby', sortOrder: 60}, //5
-      {name: 'Clothing', sortOrder: 70}, //6
+	await SubCategory.deleteMany({})
+	await Product.deleteMany({})
+    const departments = await Department.create([
+        { name: 'Groceries' },
+        { name: 'Personal Care' },
+        { name: 'Electronics' },
+        { name: 'Home Furniture' },
+        { name: 'Pets' },
+        { name: 'Baby' },
+        { name: 'Clothing' }
     ]);
+	
+	const subcategories = await SubCategory.create([
+		{ name: 'Fruits', department: departments[0]._id },
+		{ name: 'Hair Care', department: departments[1]._id },
+		{ name: 'Mobile Phones', department: departments[2]._id },
+		{ name: 'Living Room Furniture', department: departments[3]._id },
+		{ name: 'Pet Food', department: departments[4]._id },
+		{ name: 'Diapers', department: departments[5]._id },
+		{ name: 'Casual Wear', department: departments[6]._id },
+	  ]);
 
-await Product.deleteMany({});
-
-const Products = await Product.create([
+	  const Products = await Product.create([
+		{
+			name: "Apples",
+			price: 2.99,
+			image: "https://i.imgur.com/jaWvFz4.jpg",
+			manufacturer: "Farm Fresh",
+			department: departments[0]._id,
+			description: "Fresh and delicious apples.",
+			subcategory: subcategories[0]._id
+    },
+    { name: "Shampoo",
+      price: 5.49,
+      image: "https://i.imgur.com/J6r4mEn.jpg",
+      manufacturer: "CareClean",
+      department: departments[1]._id,
+      description: "Gentle and nourishing shampoo.",
+      
+      subcategory: subcategories[1]._id
+    },
     {
-        name: "Apples",
-        price: 2.99,
-        image: "https://i.imgur.com/jaWvFz4.jpg",
-        manufacturer: "Farm Fresh",
-        department: depts[0],
-        description: "Fresh and delicious apples.",
-        review: "",
-        subcategory: "Fruits"
+      name: "Smartphone",
+      price: 399.99,
+      image: "https://i.imgur.com/K3aI2Pe.jpg",
+      manufacturer: "Techtronics",
+      department: departments[2]._id,
+      description: "High-end smartphone with advanced features.",
+      
+      subcategory: subcategories[2]._id
+    },
+    {
+      name: "Sofa",
+      price: 299.00,
+      image: "https://i.imgur.com/OgkFHnb.jpg",
+      manufacturer: "ComfyLiving",
+      department: departments[3]._id,
+      description: "Comfortable and stylish sofa for your living room.",
+      
+      subcategory: subcategories[3]._id
+    },
+    {
+      name: "Dog Food",
+      price: 12.99,
+      image: "https://i.imgur.com/1CXWFkT.jpg",
+      manufacturer: "PetPal",
+      department: departments[4]._id,
+      description: "Nutritious dog food for your furry friend.",
+      
+      subcategory: subcategories[4]._id
+    },
+    {
+      name: "Baby Diapers",
+      price: 19.95,
+      image: "https://i.imgur.com/Y3fORfd.jpg",
+      manufacturer: "TinyTots",
+      department: departments[5]._id,
+      description: "Soft and absorbent diapers for babies.",
+      
+      subcategory: subcategories[5]._id
+    },
+    {
+      name: "T-Shirt",
+      price: 9.99,
+      image: "https://i.imgur.com/b3M4Qt6.jpg",
+      manufacturer: "FashionFab",
+      department: departments[6]._id,
+      description: "Casual and comfortable cotton T-shirt.",
+      
+      subcategory: subcategories[5]._id
+    },
+    {
+      name: "Orange Juice",
+      price: 3.49,
+      image: "https://i.imgur.com/q73irfP.jpg",
+      manufacturer: "JuicyCo",
+      department: departments[0]._id,
+      description: "Freshly squeezed orange juice.",
+      
+      subcategory: "Beverages"
+    },
+    {
+      name: "Toothpaste",
+      price: 2.29,
+      image: "https://i.imgur.com/mNcvPIM.jpg",
+      manufacturer: "DentalCare",
+      department: departments[1]._id,
+      description: "Fluoride toothpaste for strong teeth.",
+      
+      subcategory: "Oral Care"
+    },
+    {
+      name: "Laptop",
+      price: 799.00,
+      image: "https://i.imgur.com/XLmFnc6.jpg",
+      manufacturer: "TechGenius",
+      department: departments[2]._id,
+      description: "Powerful laptop for work and entertainment.",
+      
+      subcategory: "Computers"
       },
       {
-        name: "Shampoo",
-        price: 5.49,
-        image: "https://i.imgur.com/J6r4mEn.jpg",
-        manufacturer: "CareClean",
-        department: depts[1],
-        description: "Gentle and nourishing shampoo.",
-        review: "",
-        subcategory: "Hair Care"
+      name: "Coffee Table",
+      price: 149.99,
+      image: "https://i.imgur.com/A28sBzC.jpg",
+      manufacturer: "FurnishHome",
+      department: departments[3]._id,
+      description: "Elegant coffee table for your living room.",
+      
+      subcategory: subcategories[3]._id
       },
       {
-        name: "Smartphone",
-        price: 399.99,
-        image: "https://i.imgur.com/K3aI2Pe.jpg",
-        manufacturer: "Techtronics",
-        department: depts[2],
-        description: "High-end smartphone with advanced features.",
-        review: "",
-        subcategory: "Mobile Phones"
+      name: "Cat Food",
+      price: 8.75,
+      image,
+      manufacturer: "KittyCuisine",
+      department: departments[4]._id,
+      description: "Delicious cat food for your feline companion.",
+      
+      subcategory: subcategories[4]._id
       },
       {
-        name: "Sofa",
-        price: 299.00,
-        image: "https://i.imgur.com/OgkFHnb.jpg",
-        manufacturer: "ComfyLiving",
-        department: depts[3],
-        description: "Comfortable and stylish sofa for your living room.",
-        review: "",
-        subcategory: "Living Room Furniture"
+      name: "Baby Wipes",
+      price: 6.99,
+      image: "https://i.imgur.com/LaogAUX.jpg",
+      manufacturer: "TinyTots",
+      department: departments[5]._id,
+      description: "Gentle baby wipes for easy cleaning.",
+      
+      subcategory: "Diapering"
       },
       {
-        name: "Dog Food",
-        price: 12.99,
-        image: "https://i.imgur.com/1CXWFkT.jpg",
-        manufacturer: "PetPal",
-        department: depts[4],
-        description: "Nutritious dog food for your furry friend.",
-        review: "",
-        subcategory: "Pet Food"
+      name: "Dress Shirt",
+      price: 24.95,
+      image: "https://i.imgur.com/zaCOfM8.jpg",
+      manufacturer: "ElegantAttire",
+      department: departments[6]._id,
+      description: "Formal dress shirt for special occasions.",
+      
+      subcategory: "Formal Wear"
+      },
+      
+      {
+      name: "Bananas",
+      price: 1.49,
+      image: "https://i.imgur.com/mDlQQAz.jpg",
+      manufacturer: "TropicalHarvest",
+      department: departments[0]._id,
+      description: "Sweet and nutritious bananas.",
+      
+      subcategory: subcategories[0]._id
       },
       {
-        name: "Baby Diapers",
-        price: 19.95,
-        image: "https://i.imgur.com/Y3fORfd.jpg",
-        manufacturer: "TinyTots",
-        department: depts[5],
-        description: "Soft and absorbent diapers for babies.",
-        review: "",
-        subcategory: "Diapers"
+      name: "Conditioner",
+      price: 4.99,
+      image: "https://i.imgur.com/pLbDZpt.jpg",
+      manufacturer: "SilkyStrands",
+      department: departments[1]._id,
+      description: "Smooth and hydrating hair conditioner.",
+      
+      subcategory: subcategories[1]._id
       },
       {
-        name: "T-Shirt",
-        price: 9.99,
-        image: "https://i.imgur.com/b3M4Qt6.jpg",
-        manufacturer: "FashionFab",
-        department: depts[6],
-        description: "Casual and comfortable cotton T-shirt.",
-        review: "",
-        subcategory: "Casual Wear"
+      name: "Tablet",
+      price: 199.00,
+      image: "https://i.imgur.com/VmwOdk4.jpg",
+      manufacturer: "TechWonder",
+      department: departments[2]._id,
+      description: "Versatile tablet for work and entertainment.",
+      
+      subcategory: "Tablets"
       },
       {
-        name: "Orange Juice",
-        price: 3.49,
-        image: "https://i.imgur.com/q73irfP.jpg",
-        manufacturer: "JuicyCo",
-        department: depts[0],
-        description: "Freshly squeezed orange juice.",
-        review: "",
-        subcategory: "Beverages"
+      name: "Desk",
+      price: 149.50,
+      image: "https://i.imgur.com/UUryCVh.jpg",
+      manufacturer: "StudySolutions",
+      department: departments[3]._id,
+      description: "Functional desk for your home office.",
+      
+      subcategory: "Office Furniture"
       },
       {
-        name: "Toothpaste",
-        price: 2.29,
-        image: "https://i.imgur.com/mNcvPIM.jpg",
-        manufacturer: "DentalCare",
-        department: depts[1],
-        description: "Fluoride toothpaste for strong teeth.",
-        review: "",
-        subcategory: "Oral Care"
+      name: "Cat Litter",
+      price: 9.25,
+      image: "https://i.imgur.com/Zt9TPAK.jpg",
+      manufacturer: "KittyCuisine",
+      department: departments[4]._id,
+      description: "Odor-absorbing cat litter for easy cleanup.",
+      
+      subcategory: "Pet Supplies"
       },
       {
-        name: "Laptop",
-        price: 799.00,
-        image: "https://i.imgur.com/XLmFnc6.jpg",
-        manufacturer: "TechGenius",
-        department: depts[2],
-        description: "Powerful laptop for work and entertainment.",
-        review: "",
-        subcategory: "Computers"
+      name: "Baby Formula",
+      price: 17.99,
+      image: "https://i.imgur.com/iiwHWYp.jpg",
+      manufacturer: "TinyTots",
+      department: departments[5]._id,
+      description: "Nutrient-rich baby formula for infants.",
+      
+      subcategory: "Feeding"
       },
       {
-        name: "Coffee Table",
-        price: 149.99,
-        image: "https://i.imgur.com/A28sBzC.jpg",
-        manufacturer: "FurnishHome",
-        department: depts[3],
-        description: "Elegant coffee table for your living room.",
-        review: "",
-        subcategory: "Living Room Furniture"
+      name: "Jeans",
+      price: 29.95,
+      image: "https://i.imgur.com/qJrW28A.jpg",
+      manufacturer: "DenimDeluxe",
+      department: departments[6]._id,
+      description: "Classic denim jeans for everyday wear.",
+  
+      subcategory: "Jeans"
       },
       {
-        name: "Cat Food",
-        price: 8.75,
-        image: "",
-        manufacturer: "KittyCuisine",
-        department: depts[4],
-        description: "Delicious cat food for your feline companion.",
-        review: "",
-        subcategory: "Pet Food"
+      name: "Peaches",
+      price: 3.29,
+      image: "https://i.imgur.com/2vvDInW.jpg",
+      manufacturer: "OrchardFresh",
+      department: departments[0]._id,
+      description: "Juicy and flavorful peaches.",
+  
+      subcategory: subcategories[0]._id
       },
       {
-        name: "Baby Wipes",
-        price: 6.99,
-        image: "https://i.imgur.com/LaogAUX.jpg",
-        manufacturer: "TinyTots",
-        department: depts[5],
-        description: "Gentle baby wipes for easy cleaning.",
-        review: "",
-        subcategory: "Diapering"
+      name: "Deodorant",
+      price: 3.75,
+      image: "https://i.imgur.com/1Mj9Dvf.jpg",
+      manufacturer: "FreshGlow",
+      department: departments[1]._id,
+      description: "Long-lasting deodorant wbluetith a pleasant scent.",
+ 
+      subcategory: "Personal Care"
       },
       {
-        name: "Dress Shirt",
-        price: 24.95,
-        image: "https://i.imgur.com/zaCOfM8.jpg",
-        manufacturer: "ElegantAttire",
-        department: depts[6],
-        description: "Formal dress shirt for special occasions.",
-        review: "",
-        subcategory: "Formal Wear"
-      },
-
-      {
-        name: "Bananas",
-        price: 1.49,
-        image: "https://i.imgur.com/mDlQQAz.jpg",
-        manufacturer: "TropicalHarvest",
-        department: depts[0],
-        description: "Sweet and nutritious bananas.",
-        review: "",
-        subcategory: "Fruits"
-      },
-      {
-        name: "Conditioner",
-        price: 4.99,
-        image: "https://i.imgur.com/pLbDZpt.jpg",
-        manufacturer: "SilkyStrands",
-        department: depts[1],
-        description: "Smooth and hydrating hair conditioner.",
-        review: "",
-        subcategory: "Hair Care"
-      },
-      {
-        name: "Tablet",
-        price: 199.00,
-        image: "https://i.imgur.com/VmwOdk4.jpg",
-        manufacturer: "TechWonder",
-        department: depts[2],
-        description: "Versatile tablet for work and entertainment.",
-        review: "",
-        subcategory: "Tablets"
-      },
-      {
-        name: "Desk",
-        price: 149.50,
-        image: "https://i.imgur.com/UUryCVh.jpg",
-        manufacturer: "StudySolutions",
-        department: depts[3],
-        description: "Functional desk for your home office.",
-        review: "",
-        subcategory: "Office Furniture"
-      },
-      {
-        name: "Cat Litter",
-        price: 9.25,
-        image: "https://i.imgur.com/Zt9TPAK.jpg",
-        manufacturer: "KittyCuisine",
-        department: depts[4],
-        description: "Odor-absorbing cat litter for easy cleanup.",
-        review: "",
-        subcategory: "Pet Supplies"
-      },
-      {
-        name: "Baby Formula",
-        price: 17.99,
-        image: "https://i.imgur.com/iiwHWYp.jpg",
-        manufacturer: "TinyTots",
-        department: depts[5],
-        description: "Nutrient-rich baby formula for infants.",
-        review: "",
-        subcategory: "Feeding"
-      },
-      {
-        name: "Jeans",
-        price: 29.95,
-        image: "https://i.imgur.com/qJrW28A.jpg",
-        manufacturer: "DenimDeluxe",
-        department: depts[6],
-        description: "Classic denim jeans for everyday wear.",
-        review: "",
-        subcategory: "Jeans"
-      },
-      {
-        name: "Peaches",
-        price: 3.29,
-        image: "https://i.imgur.com/2vvDInW.jpg",
-        manufacturer: "OrchardFresh",
-        department: depts[0],
-        description: "Juicy and flavorful peaches.",
-        review: "",
-        subcategory: "Fruits"
-      },
-      {
-        name: "Deodorant",
-        price: 3.75,
-        image: "https://i.imgur.com/1Mj9Dvf.jpg",
-        manufacturer: "FreshGlow",
-        department: depts[1],
-        description: "Long-lasting deodorant wbluetith a pleasant scent.",
-        review: "",
-        subcategory: "Personal Care"
-      },
-      {
-        name: "Bluetooth Speaker",
-        price: 59.99,
-        image: "https://i.imgur.com/MnTUHX8.jpg",
-        manufacturer: "SonicSound",
-        department: depts[2],
-        description: "Compact Bluetooth speaker for wireless audio.",
-        review: "",
-        subcategory: "Speakers"
+      name: "Bluetooth Speaker",
+      price: 59.99,
+      image: "https://i.imgur.com/MnTUHX8.jpg",
+      manufacturer: "SonicSound",
+      department: departments[2]._id,
+      description: "Compact Bluetooth speaker for wireless audio.",
+  
+      subcategory: "Speakers"
       }
 
-    
-]);
 
-process.exit();
+    ])
 
-})();
+        process.exit();
+    })();
+
