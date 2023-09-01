447a7aa930f57860f3c812803791e0c14d63c045
Jose Monagas <josemonagas212@gmail.com>
2023-09-01 19:59:05 -0400

    Update index.js

--- Fallmart/src/index.js
+++ Fallmart/src/index.js
@@ -15,19 +15,4 @@ root.render(
 	</StrictMode>
 );
 
-// // index.js
-// import './styles.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './pages/App/App';
 
-// import { BrowserRouter as Router } from 'react-router-dom';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Router>
-//       <App />
-//     </Router>
-//   </React.StrictMode>
-// );
