b157bceeac62feeb230195add39012a30e195c61
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-09-01 20:03:51 -0400

    tested orders route

--- Fallmart/routes/api/orders.js
+++ Fallmart/routes/api/orders.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ordersCtrl = require('../../controllers/api/orders');
-
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 // GET /api/orders/cart
 router.get('/cart', ordersCtrl.cart);
 // GET /api/orders/history
-router.get('/history', ordersCtrl.history);
+router.get('/history', ensureLoggedIn, ordersCtrl.history);
 // POST /api/orders/cart/items/:id
 router.post('/cart/items/:id', ordersCtrl.addToCart);
 // POST /api/orders/cart/checkout
@@ -13,8 +13,8 @@ router.post('/cart/checkout', ordersCtrl.checkout);
 // POST /api/orders/cart/qty
 router.put('/cart/qty', ordersCtrl.setItemQtyInCart);
 // PUT /api/orders/cart/coupon
-router.put('/cart/coupon', ordersCtrl.applyCoupon);
+router.put('/cart/coupon', ensureLoggedIn, ordersCtrl.applyCoupon);
 // DELETE /api.orders/cart/remove
-router.delete('/cart/remove', ordersCtrl.removeFromCart);
+router.delete('/cart/remove/:id', ordersCtrl.removeFromCart);
 
 module.exports = router;
