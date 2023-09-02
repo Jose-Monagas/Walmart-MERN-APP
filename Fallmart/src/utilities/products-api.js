fb045bcbcc2c79fe29126d6aa4d33a082fcc19af
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-09-02 17:32:01 -0400

    fixed utilities

--- Fallmart/src/utilities/products-api.js
+++ Fallmart/src/utilities/products-api.js
@@ -3,10 +3,12 @@ import Product from '../../models/product';
 
 const BASE_URL = '/api/products';
 
-export function getProducts() {
-	return sendRequest(BASE_URL);
+export function getProductsBySubCategoryId(subcategoryId) {
+	return sendRequest(`${BASE_URL}/${subcategoryId}`);
 }
 
 export function getProductById(productId) {
-	return sendRequest(`${BASE_URL}/${productId}`);
+	return sendRequest(`${BASE_URL}/product/${productId}`);
 }
+
+module.exports = router;
--- Fallmart/src/utilities/subCategories-api.js
+++ Fallmart/src/utilities/subCategories-api.js
@@ -2,18 +2,6 @@ import sendRequest from './send-request';
 
 const BASE_URL = 'api/subcategories';
 
-export function listAllSubcategories(subCategoryList) {
-	return sendRequest(
-		`${BASE_URL}/department/${departmentId}`,
-		'GET',
-		subCategoryList
-	);
-}
-
-export function listProductsInSubCategory(subCategoryProducts) {
-	return sendRequest(
-		`${BASE_URL}/products/${subCategoryId}`,
-		'GET',
-		subCategoryProducts
-	);
+export function listAllSubcategoriesByDeptId(departmentId) {
+	return sendRequest(`${BASE_URL}/department/${departmentId}`, 'GET');
 }
