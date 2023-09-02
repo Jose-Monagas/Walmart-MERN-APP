6e02b0bbde80d19d93bb4f7259376d03468fd908
Graehm <gpatrellfazio@gmail.com>
2023-09-01 21:21:13 -0500

    all strings and filled out

--- Fallmart/config/seed.js
+++ Fallmart/config/seed.js
@@ -19,22 +19,22 @@ const Product = require('../models/product');
     ]);
 	
 	const subcategories = await SubCategory.create([
-		{ name: 'Fruits', products: ['Apples', 'Bananas', 'Peaches'], department: ['']},
-		{ name: 'Hair Care', products: ['Shampoo', 'Conditioner'], department: ['']},
-		{ name: 'Mobile Phones', products: ['Smartphone'], department: ['']},
-		{ name: 'Living Room Furniture', products: ['Sofa', 'Coffee Table'], department: ['']},
-		{ name: 'Pet Food', products: ['Dog Food', 'Cat Food'], department: ['']},
-		{ name: 'Diapers', products: ['Baby Diapers', 'Baby Wipes'], department: ['']},
-		{ name: 'Casual Wear', products: ['T-Shirt'], department: ['']},
-    { name: 'Beverages', products: ['Orange Juice'], department: ['']},
-    { name: 'Oral Care', products: ['Toothpaste'], department: ['']},
-    { name: 'Computers', products: ['Laptop'], department: ['']},
-    { name: 'Tablets', products: ['Tablet'], department: ['']},
-    { name: 'Wipes', products: [], department: ['']},
-    { name: 'Formal Wear', products: ['Dress Shirt'], department: ['']},
-    { name: 'Office Furniture', products: ['Desk'], department: ['']},
-    { name: 'Antiperspirants', products: [], department: ['']},
-    { name: 'Speakers', products: [], department: ['']},
+		{ name: 'Fruits', products: ['Apples', 'Bananas', 'Peaches'], department: ['Groceries']},
+		{ name: 'Hair Care', products: ['Shampoo', 'Conditioner'], department: ['Personal Care']},
+		{ name: 'Mobile Phones', products: ['Smartphone'], department: ['Electronics']},
+		{ name: 'Living Room Furniture', products: ['Sofa', 'Coffee Table'], department: ['Furniture']},
+		{ name: 'Pet Food', products: ['Dog Food', 'Cat Food', 'Cat Litter'], department: ['Pet Supplies']},
+		{ name: 'Diapers', products: ['Baby Diapers', 'Baby Wipes'], department: ['Baby Supplies']},
+		{ name: 'Casual Wear', products: ['T-Shirt', 'Jeans'], department: ['Clothing']},
+    { name: 'Beverages', products: ['Orange Juice'], department: ['Groceries']},
+    { name: 'Oral Care', products: ['Toothpaste'], department: ['Personal Care']},
+    { name: 'Computers', products: ['Laptop'], department: ['Electronics']},
+    { name: 'Tablets', products: ['Tablet'], department: ['Electronics']},
+    { name: 'Wipes', products: ['Baby Formula', 'Baby Wipes'], department: ['Baby Supplies']},
+    { name: 'Formal Wear', products: ['Dress Shirt'], department: ['Clothing']},
+    { name: 'Office Furniture', products: ['Desk'], department: ['Furniture']},
+    { name: 'Antiperspirants', products: ['Deodorant'], department: ['Personal Care']},
+    { name: 'Speakers', products: ['Bluetooth Speaker'], department: ['Electronics']},
 	  ]);
 
 	  const products = await Product.create([
@@ -43,139 +43,139 @@ const Product = require('../models/product');
 			price: 2.99,
 			image: "https://i.imgur.com/jaWvFz4.jpg",
 			manufacturer: "Farm Fresh",
-			department: '',
+			department: 'Groceries',
 			description: "Fresh and delicious apples.",
       review: '',
-			subcategory: ''
+			subcategory: 'Fruits'
     },
     { name: "Shampoo",
       price: 5.49,
       image: "https://i.imgur.com/J6r4mEn.jpg",
       manufacturer: "CareClean",
-      department: '',
+      department: 'Personal Care',
       description: "Gentle and nourishing shampoo.",
       review: '',
-      subcategory: ''
+      subcategory: 'Hair care'
     },
     {
       name: "Smartphone",
       price: 399.99,
       image: "https://i.imgur.com/K3aI2Pe.jpg",
       manufacturer: "Techtronics",
-      department: '',
+      department: 'Electronics',
       description: "High-end smartphone with advanced features.",
       review: '',
-      subcategory: ''
+      subcategory: 'Mobile Phones'
     },
     {
       name: "Sofa",
       price: 299.00,
       image: "https://i.imgur.com/OgkFHnb.jpg",
       manufacturer: "ComfyLiving",
-      department: '',
+      department: 'Furniture',
       description: "Comfortable and stylish sofa for your living room.",
       review: '',
-      subcategory: ''
+      subcategory: 'Living Room Furniture'
     },
     {
       name: "Dog Food",
       price: 12.99,
       image: "https://i.imgur.com/1CXWFkT.jpg",
       manufacturer: "PetPal",
-      department: '',
+      department: 'Pet Supplies',
       description: "Nutritious dog food for your furry friend.",
       review: '',
-      subcategory: ''
+      subcategory: 'Pet Food'
     },
     {
       name: "Baby Diapers",
       price: 19.95,
       image: "https://i.imgur.com/Y3fORfd.jpg",
       manufacturer: "TinyTots",
-      department: '',
+      department: 'Baby Supplies',
       description: "Soft and absorbent diapers for babies.",
       review: '',
-      subcategory: ''
+      subcategory: 'Diapers'
     },
     {
       name: "T-Shirt",
       price: 9.99,
       image: "https://i.imgur.com/b3M4Qt6.jpg",
       manufacturer: "FashionFab",
-      department: '',
+      department: 'Clothing',
       description: "Casual and comfortable cotton T-shirt.",
       review: '',
-      subcategory: ''
+      subcategory: 'Casual Wear'
     },
     {
       name: "Orange Juice",
       price: 3.49,
       image: "https://i.imgur.com/q73irfP.jpg",
       manufacturer: "JuicyCo",
-      department: '',
+      department: 'Groceries',
       description: "Freshly squeezed orange juice.",
       review: '',
-      subcategory: ''
+      subcategory: 'Baverages'
     },
     {
       name: "Toothpaste",
       price: 2.29,
       image: "https://i.imgur.com/mNcvPIM.jpg",
       manufacturer: "DentalCare",
-      department: '',
+      department: 'Personal Care',
       description: "Fluoride toothpaste for strong teeth.",
       review: '',
-      subcategory: ''
+      subcategory: 'Oral Care'
     },
     {
       name: "Laptop",
       price: 799.00,
       image: "https://i.imgur.com/XLmFnc6.jpg",
       manufacturer: "TechGenius",
-      department: '',
+      department: 'Electronics',
       description: "Powerful laptop for work and entertainment.",
       review: '',
-      subcategory: ''
+      subcategory: 'Computers'
       },
       {
       name: "Coffee Table",
       price: 149.99,
       image: "https://i.imgur.com/A28sBzC.jpg",
       manufacturer: "FurnishHome",
-      department: '',
+      department: 'Furniture',
       description: "Elegant coffee table for your living room.",
       review: '',
-      subcategory: ''
+      subcategory: 'Living Room Funiture'
       },
       {
       name: "Cat Food",
       price: 8.75,
       image: "https://i.imgur.com/0p5fAym.jpg",
       manufacturer: "KittyCuisine",
-      department: '',
+      department: 'Pet Supplies',
       description: "Delicious cat food for your feline companion.",
       review: '',
-      subcategory: ''
+      subcategory: 'Pet Food'
       },
       {
       name: "Baby Wipes",
       price: 6.99,
       image: "https://i.imgur.com/LaogAUX.jpg",
       manufacturer: "TinyTots",
-      department: '',
+      department: 'Baby Supplies',
       description: "Gentle baby wipes for easy cleaning.",
       review: '',
-      subcategory: ''
+      subcategory: 'Wipes'
       },
       {
       name: "Dress Shirt",
       price: 24.95,
       image: "https://i.imgur.com/zaCOfM8.jpg",
       manufacturer: "ElegantAttire",
-      department: '',
+      department: 'Clothing',
       description: "Formal dress shirt for special occasions.",
       review: '',
-      subcategory: ''
+      subcategory: 'Formal Wear'
       },
       
       {
@@ -183,100 +183,100 @@ const Product = require('../models/product');
       price: 1.49,
       image: "https://i.imgur.com/mDlQQAz.jpg",
       manufacturer: "TropicalHarvest",
-      department: '',
+      department: 'Groceries',
       description: "Sweet and nutritious bananas.",
       review: '',
-      subcategory: ''
+      subcategory: 'Fruits'
       },
       {
       name: "Conditioner",
       price: 4.99,
       image: "https://i.imgur.com/pLbDZpt.jpg",
       manufacturer: "SilkyStrands",
-      department: '',
+      department: 'Personal Care',
       description: "Smooth and hydrating hair conditioner.",
       review: '',
-      subcategory: ''
+      subcategory: 'Hair Care'
       },
       {
       name: "Tablet",
       price: 199.00,
       image: "https://i.imgur.com/VmwOdk4.jpg",
       manufacturer: "TechWonder",
-      department: '',
+      department: 'Electronics',
       description: "Versatile tablet for work and entertainment.",
       review: '',
-      subcategory: ''
+      subcategory: 'Tablets'
       },
       {
       name: "Desk",
       price: 149.50,
       image: "https://i.imgur.com/UUryCVh.jpg",
       manufacturer: "StudySolutions",
-      department: '',
+      department: 'Furniture',
       description: "Functional desk for your home office.",
       review: '',
-      subcategory: ''
+      subcategory: 'Office Furniture'
       },
       {
       name: "Cat Litter",
       price: 9.25,
       image: "https://i.imgur.com/Zt9TPAK.jpg",
       manufacturer: "KittyCuisine",
-      department: '',
+      department: 'Pet Supplies',
       description: "Odor-absorbing cat litter for easy cleanup.",
       review: '',
-      subcategory: "Pet Supplies"
+      subcategory: "Pet Food"
       },
       {
       name: "Baby Formula",
       price: 17.99,
       image: "https://i.imgur.com/iiwHWYp.jpg",
       manufacturer: "TinyTots",
-      department: '',
+      department: 'Baby Supplies',
       description: "Nutrient-rich baby formula for infants.",
       review: '',
-      subcategory: "Feeding"
+      subcategory: "Wipes"
       },
       {
       name: "Jeans",
       price: 29.95,
       image: "https://i.imgur.com/qJrW28A.jpg",
       manufacturer: "DenimDeluxe",
-      department: '',
+      department: 'Clothing',
       description: "Classic denim jeans for everyday wear.",
       review: '',
-      subcategory: "Jeans"
+      subcategory: "Casual Wear"
       },
       {
       name: "Peaches",
       price: 3.29,
       image: "https://i.imgur.com/2vvDInW.jpg",
       manufacturer: "OrchardFresh",
-      department: '',
+      department: 'Groceries',
       description: "Juicy and flavorful peaches.",
       review: '',
-      subcategory: ''
+      subcategory: 'Fruits'
       },
       {
       name: "Deodorant",
       price: 3.75,
       image: "https://i.imgur.com/1Mj9Dvf.jpg",
       manufacturer: "FreshGlow",
-      department: '',
+      department: 'Personal Care',
       description: "Long-lasting deodorant wbluetith a pleasant scent.",
       review: '',
-      subcategory: ''
+      subcategory: 'Antiperspirants'
       },
       {
       name: "Bluetooth Speaker",
       price: 59.99,
       image: "https://i.imgur.com/MnTUHX8.jpg",
       manufacturer: "SonicSound",
-      department: '',
+      department: 'Electronics',
       description: "Compact Bluetooth speaker for wireless audio.",
       review: '',
-      subcategory: ''
+      subcategory: 'Computers'
       }
 
 
