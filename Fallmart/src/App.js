b26baee87632b97ddaa2fcdef3c647e86d548226
SriD647 <sridatt.rao@hotmail.com>
2023-09-02 16:19:43 -0400

    app still works...made changes to app.js but commented it out for now

--- Fallmart/src/App.js
+++ Fallmart/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+
 // import styles from './App.module.scss';
 import NavBar from './components/NavBar/NavBar';
 import HeaderBanner from './components/HeaderBanner/HeaderBanner';
@@ -9,8 +10,27 @@ import SecondNav from './components/SecondNav/SecondNav';
 import './styles.scss';
 import HomeImage from './components/HomeImage/HomeImage';
 import Furniture from './pages/Furniture/Furniture';
+import DepartmentList from './components/DepartmentList/DepartmentList';
+
+
 
 export default function App() {
+    // const [departments, setDepartments] = useState([]);
+
+	// const fetchDepts = async () => {
+	// 	try {
+	// 	  const response = await fetch('/api/department/showdepts'); 
+	// 	  const data = await response.json();
+	// 	  setDepartments(data);
+	// 	} catch (error) {
+	// 	  console.error(error);
+	// 	}
+	//   };
+	  
+    // useEffect(() => {
+	// 	fetchDepts();
+	//   }, []);
+
 	return (
 		<div className="App">
 			<HeaderBanner />
