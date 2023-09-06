3bcd0730660a1b59562079463c4312cbc1438f75
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-09-05 23:37:08 -0400

    fixed sub cat utility route

--- Fallmart/src/utilities/subCategories-api.js
+++ Fallmart/src/utilities/subCategories-api.js
@@ -3,8 +3,9 @@ import sendRequest from './send-request';
 const BASE_URL = 'api/subcategories';
 
 export function listAllSubcategoriesByDeptId(departmentId) {
-	// TODO: broken, remove deparment/
-	return sendRequest(`${BASE_URL}/department/${departmentId}`, 'GET');
+	// TODO: broken, remove deparment/:
+	//got it
+	return sendRequest(`${BASE_URL}/${departmentId}`, 'GET');
 }
 
 export function listAllSubcategories() {
