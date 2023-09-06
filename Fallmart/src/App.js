8ecc02f2f781f487bf0d5aa26676d5f7790e0b4a
Jose Monagas <josemonagas212@gmail.com>
2023-09-06 19:20:15 -0400

    Revert "Jun/newnav"

--- Fallmart/src/App.js
+++ Fallmart/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // import styles from './App.module.scss';
 import NavBar from './components/NavBar/NavBar';
 import HeaderBanner from './components/HeaderBanner/HeaderBanner';
-import PrimaryNav from './components/PrimaryNav/PrimaryNav';
+import DepartmentList from './components/DepartmentList/DepartmentList';
 import SecondNav from './components/SecondNav/SecondNav';
 import './styles.scss';
 import HomeImage from './components/HomeImage/HomeImage';
@@ -28,7 +28,9 @@ export default function App() {
 
 	return (
 		<div className="App">
+			<HeaderBanner />
 			<NavBar />
+			<SecondNav />
 			<DepartmentList departments={departments} />
 			<Routes>
 				<Route path="/" element={<HomeImage />} />
--- Fallmart/src/components/NavBar/NavBar.js
+++ Fallmart/src/components/NavBar/NavBar.js
@@ -11,39 +11,28 @@ function NavBar() {
 
 	return (
 		<nav className={styles.header_core}>
-			{/* main logo */}
-			<div className={styles.logo_core}>
+			<div className={styles.left_core}>
+				<div className={styles.search_container}>
+					<input
+						className={styles.header_searchInput}
+						type="text"
+						placeholder="What can we help you find?"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+					/>
+					<button className={styles.search_button} onClick={handleSearch}>
+						<FaSearch />
+					</button>
+				</div>
+			</div>
+			<div className={styles.logomiddle_core}>
 				<img src="https://i.imgur.com/xgA0d20.png" alt="" />
 			</div>
-
-			{/* Search box */}
-			<div className={styles.search_container}>
-				<input
-					className={styles.header_searchInput}
-					type="text"
-					placeholder="Search everything at Fallmart online and in store"
-					value={searchValue}
-					onChange={(e) => setSearchValue(e.target.value)}
-				/>
-				<button className={styles.search_button} onClick={handleSearch}>
-					<FaSearch />
-				</button>
-			</div>
-
-			{/* 4 links to the right */}
 			<div className={styles.right_core}>
 				<div className={styles.account}>
-					<a href="/account/order-tracking"> Orders </a>
+				<a href="/account/order-tracking"> Orders </a>
 					<span> & </span>
 					<button className={styles.signbutton}> Sign In</button>
-				</div>
-				<div className={styles.humanicon}>
-					<img
-						src="https://i.imgur.com/l3cX5Ev.png"
-						alt=""
-						width="20"
-						height="20"
-					/>
 				</div>
 				<div className={styles.location}>
 					<img
@@ -53,20 +42,28 @@ function NavBar() {
 						height="30"
 					/>
 				</div>
+				<div className={styles.humanicon}>
+					<img
+						src="https://i.imgur.com/l3cX5Ev.png"
+						alt=""
+						width="30"
+						height="30"
+					/>
+				</div>
 				<div className={styles.favorites}>
 					<img
 						src="https://i.imgur.com/UpoCDeP.png"
 						alt=""
-						width="20"
-						height="20"
+						width="22"
+						height="22"
 					/>
 				</div>
 				<div className={styles.cart}>
 					<img
 						src="https://i.imgur.com/TWI8Zuk.png"
 						alt=""
-						width="20"
-						height="20"
+						width="22"
+						height="22"
 					/>
 				</div>
 			</div>
