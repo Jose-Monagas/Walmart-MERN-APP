b1a93dbaba1fd980673833aca374d2637ac14edf
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-09-07 19:35:25 -0400

    sign inworks

--- Fallmart/src/components/ProductTile/ProductTile.js
+++ Fallmart/src/components/ProductTile/ProductTile.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import styles from './ProductTile.module.scss';
-function ProductTile({ product }) {
-  const [isFavorite, setIsFavorite] = useState(false);
-  const toggleFavorite = () => {
-    setIsFavorite(!isFavorite);
-  };
-  return (
-    <div className={styles.product_tile}>
-      <div className={styles.favorite_button} onClick={toggleFavorite}>
-	  {isFavorite ? '❤️' : '🤍'}
-      </div>
-      <h3 className={styles.product_name}>{product.name}</h3>
-      <img src={product.image} height={'110px'} width={'100px'} alt={product.name} />
-      <h4 className={styles.product_price}>${product.price}</h4>
-      <h4 className={styles.product_sold}>Sold by: {product.manufacturer}</h4>
-      <button className={styles.cart_button}>ADD TO CART</button>
-    </div>
-  );
+function ProductTile({ product, setFavoriteCount, favoriteCount }) {
+	const [isFavorite, setIsFavorite] = useState(false);
+	const toggleFavorite = () => {
+		setIsFavorite(!isFavorite);
+		setFavoriteCount(isFavorite ? favoriteCount - 1 : favoriteCount + 1);
+	};
+	return (
+		<div className={styles.product_tile}>
+			<div className={styles.favorite_button} onClick={toggleFavorite}>
+				{isFavorite ? '❤️' : '🤍'}
+			</div>
+			<h3 className={styles.product_name}>{product.name}</h3>
+			<img
+				src={product.image}
+				height={'110px'}
+				width={'100px'}
+				alt={product.name}
+			/>
+			<h4 className={styles.product_price}>${product.price}</h4>
+			<h4 className={styles.product_sold}>Sold by: {product.manufacturer}</h4>
+			<button className={styles.cart_button}>ADD TO CART</button>
+		</div>
+	);
 }
 export default ProductTile;
