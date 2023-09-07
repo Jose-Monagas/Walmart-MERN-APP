d1555b2888394cb4e8a277cc2c4395b2e81e3bce
SriD647 <sridatt.rao@hotmail.com>
2023-09-07 19:22:37 -0400

    updated home page...still lot of work to do

--- Fallmart/src/components/HomeImage/HomeImage.js
+++ Fallmart/src/components/HomeImage/HomeImage.js
@@ -2,20 +2,16 @@ import React from 'react';
 import styles from './HomeImage.module.scss';
 
 function HomeImage() {
-	return (
-		<div className={styles.allimage}>
-			<img
-				src="https://i.imgur.com/eflVxXr.jpg"
-				alt=""
-				// width="100%"
-			/>
-			<img
-				src="https://i.imgur.com/frxnxYF.jpg"
-				alt=""
-				// width="100%"
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.grid}>
+      <img src="img/sofa.jpeg" className="galleryImage" alt="Image 1" />
+	  <img src="img/suit.jpeg" className="galleryImage" alt="Image 2" />	
+	  <img src="img/cellphones.jpeg" className="galleryImage" alt="Image 3" />
+	  <img src="img/dog.jpeg" className="galleryImage" alt="Image 4" />
+	  <img src="img/tv.jpeg" className="galleryImage" alt="Image 5" />
+	  <img src="img/table.jpeg" className="galleryImage" alt="Image 6" />
+    </div>
+  );
 }
 
 export default HomeImage;
