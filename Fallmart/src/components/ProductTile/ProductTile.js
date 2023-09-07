85124fe87b4df5e592d5adcd9e54309897969e7b
jib5549 <jib5549@gmail.com>
2023-09-07 08:46:08 -0400

    got the heart to show up on every top right corner of every item and items show up next to each other horizontally

--- Fallmart/src/components/ProductTile/ProductTile.js
+++ Fallmart/src/components/ProductTile/ProductTile.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './ProductTile.module.scss';
 
 function ProductTile({ product }) {
-	return (
-		<>
-			<h3>{product.name}</h3>
-			<img src={product.image} height={'110px'} width={'100px'}></img>
-			<h4>${product.price}</h4>
-			<h4>Sold by: {product.manufacturer}</h4>
-			<button>add to cart</button>
-		</>
-	);
+
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <div className={styles.product_tile}>
+      <div className={styles.favorite_button} onClick={toggleFavorite}>
+        {isFavorite ? '❤️' : '🤍'} 
+      </div>
+      <h3>{product.name}</h3>
+      <img src={product.image} height={'110px'} width={'100px'} alt={product.name} />
+      <h4>${product.price}</h4>
+      <h4 className={styles.productSold}>Sold by: {product.manufacturer}</h4>
+      <button className={styles.cart_button}>add to cart</button>
+    </div>
+  );
 }
 
 export default ProductTile;
