97f7a81eb274012720c7ff6d19429103d1e7a4db
Jose <josemonagas212@gmail.com>
2023-09-06 20:08:21 -0400

    feat: restructure routes to use dynamic params

--- Fallmart/src/App.js
+++ Fallmart/src/App.js
@@ -34,60 +34,7 @@ export default function App() {
 			<DepartmentList departments={departments} />
 			<Routes>
 				<Route path="/" element={<HomeImage />} />
-				<Route
-					path="/living-room-furniture"
-					element={<SubcategoryPage name="Living Room Furniture" />}
-				/>
-				<Route path="/fruits" element={<SubcategoryPage name="Fruits" />} />
-				<Route
-					path="/hair-care"
-					element={<SubcategoryPage name="Hair Care" />}
-				/>
-				<Route path="/diapers" element={<SubcategoryPage name="Diapers" />} />
-				<Route
-					path="/formal-wear"
-					element={<SubcategoryPage name="Formal Wear" />}
-				/>
-				<Route path="/wipes" element={<SubcategoryPage name="Wipes" />} />
-				<Route
-					path="/casual-wear"
-					element={<SubcategoryPage name="Casual Wear" />}
-				/>
-				<Route
-					path="/formal-wear"
-					element={<SubcategoryPage name="Formal Wear" />}
-				/>
-				<Route
-					path="/mobile-phones"
-					element={<SubcategoryPage name="Mobile Phones" />}
-				/>
-				<Route
-					path="/computers"
-					element={<SubcategoryPage name="Computers" />}
-				/>
-				<Route path="/tablets" element={<SubcategoryPage name="Tablets" />} />
-				<Route path="/speakers" element={<SubcategoryPage name="Speakers" />} />
-				<Route
-					path="/beverages"
-					element={<SubcategoryPage name="Beverages" />}
-				/>
-				<Route
-					path="/office-furniture"
-					element={<SubcategoryPage name="Office Furniture" />}
-				/>
-				<Route
-					path="/oral-care"
-					element={<SubcategoryPage name="Oral Care" />}
-				/>
-				<Route
-					path="/hair-care"
-					element={<SubcategoryPage name="Hair Care" />}
-				/>
-				<Route
-					path="/antiperspirants"
-					element={<SubcategoryPage name="Anstiperspirants" />}
-				/>
-				<Route path="/pet-food" element={<SubcategoryPage name="Pet Food" />} />
+				<Route path="/:name" element={<SubcategoryPage />} />
 			</Routes>
 		</div>
 	);
